42cf5fd4aa63b6054cc547bacc709258a20ac66b
Alex Geis <siegxela@gmail.com>
2022-12-21 18:18:48 -0700

    style work in progress page

--- vite-portfolio/src/pages/WIP/WIP.tsx
+++ vite-portfolio/src/pages/WIP/WIP.tsx
@@ -1,10 +1,12 @@
+import styles from "./WIP.module.css"
+
 export function WorkInProgressPage() {
 
   return (
-    <div>
+    <div className={styles.wrapper}>
       <h1>Oops!</h1>
       <p>Sorry, this page will be finished soon.</p>
-      <p>In the meantime, please listen to some <a href="https://open.spotify.com/artist/41yri20ft220T9dx4A6mWa?si=-e-vKZuXSvqnzf3-7tIjDA">music</a></p>
+      <p>In the meantime, relax with some <a href="https://open.spotify.com/artist/41yri20ft220T9dx4A6mWa?si=-e-vKZuXSvqnzf3-7tIjDA">music</a></p>
     </div>
   );
 }
