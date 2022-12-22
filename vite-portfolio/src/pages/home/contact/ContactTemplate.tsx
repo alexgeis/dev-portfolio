240d4c089f5a952d38426330be434e24cfe03e8a
Alex Geis <siegxela@gmail.com>
2022-12-21 18:30:29 -0700

    NavLink with react router the concat button in home contact section near footer

--- vite-portfolio/src/pages/home/contact/ContactTemplate.tsx
+++ vite-portfolio/src/pages/home/contact/ContactTemplate.tsx
@@ -1,10 +1,13 @@
 import styles from "./ContactTemplate.module.css";
-
+import { NavLink } from "react-router-dom";
 
 export const ContactTemplate = () => {
 	return <div className={styles.wrapper}>
 				<h1 className={styles.contactTitle}>Have a project in mind?</h1>
 				<p className={styles.contactDesc}>I'm available to work!</p>
-				<p className={styles.contactBtn}>Contact Me</p>
+				<NavLink 
+                    to="contact">
+					<p className={styles.contactBtn}>Contact Me</p>
+                </NavLink>
 		   </div>;
 };
