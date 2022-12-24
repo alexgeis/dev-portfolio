8399d2935c0f9877e707a12d3bebec996146c90f
Alex Geis <siegxela@gmail.com>
2022-12-24 11:49:43 -0700

    resave files with new prettier config

--- vite-portfolio/src/main.tsx
+++ vite-portfolio/src/main.tsx
@@ -1,46 +1,43 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { AppTemplate } from './App'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { AppTemplate } from "./App";
 import { Player } from "./pages/music/PlayerApp";
 import { tracks } from "./assets/audio/audio";
-import { ContactPage } from './pages/contact/ContactPage';
-import { WorkInProgressPage } from './pages/WIP/WIP';
-import { ErrorPage } from './pages/error/ErrorPage'
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
-import './index.css'
-import { HomePage } from './pages/home/HomePage';
+import { ContactPage } from "./pages/contact/ContactPage";
+import { WorkInProgressPage } from "./pages/WIP/WIP";
+import { ErrorPage } from "./pages/error/ErrorPage";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
+import { HomePage } from "./pages/home/HomePage";
 
 const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <AppTemplate />,
-    errorElement: <ErrorPage />,
-    children: [
-      {
-        path: "/",
-        element: <HomePage />,
-      },
-      {
-        path: "projects",
-        element: <WorkInProgressPage />,
-      },
-      {
-        path: "music",
-        element: <Player trackList={tracks} />,
-      },
-      {
-        path: "contact",
-        element: <ContactPage />,
-      },
-    ],
-  }
+	{
+		path: "/",
+		element: <AppTemplate />,
+		errorElement: <ErrorPage />,
+		children: [
+			{
+				path: "/",
+				element: <HomePage />,
+			},
+			{
+				path: "projects",
+				element: <WorkInProgressPage />,
+			},
+			{
+				path: "music",
+				element: <Player trackList={tracks} />,
+			},
+			{
+				path: "contact",
+				element: <ContactPage />,
+			},
+		],
+	},
 ]);
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+	<React.StrictMode>
+		<RouterProvider router={router} />
+	</React.StrictMode>
+);
