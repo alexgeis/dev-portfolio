f0d4bb1a5f8f0d3d3c0cb209ff06555246eef2d9
Alex Geis <siegxela@gmail.com>
2022-12-23 17:23:36 -0700

    remove unneeded imports from contactPage.tsx

--- vite-portfolio/src/pages/contact/ContactPage.tsx
+++ vite-portfolio/src/pages/contact/ContactPage.tsx
@@ -4,24 +4,6 @@ import { useState, useEffect, useRef } from "react";
 import { ContactPageTemplate } from "./ContactPageTemplate";
 import { ContactFormTemplate } from "./ContactFormTemplate";
 
-// section title
-import { SectionTitle } from "./sectionItems/SectionTitle";
-import { SectionBtn } from "./sectionItems/SectionBtn";
-
-// projects
-import { ProjectTemplate } from "./projects/ProjectTemplate"
-// import { ProjectItem } from "./projects/ProjectItem"
-import { ProjectCard } from "./projects/ProjectCard"
-import { ProjectCardItem } from "./projects/ProjectCardItem"
-import { projectData } from "./projects/projectData";
-
-// contact
-import { ContactTemplate } from "./contact/ContactTemplate"
-// import { ContactItem } from "./contact/ContactItem"
-// icons
-// import loopCurrentBtn from "../../assets/icons/player/loop_current.png";
-
-// const fmtMSS = (s) => new Date(1000 * s).toISOString().substr(15, 4);
 const fmtMSS = (s: number) => new Date(1000 * s).toISOString().substring(15, 19);
 
 export const ContactPage = () => {
