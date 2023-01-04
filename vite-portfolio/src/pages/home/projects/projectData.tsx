0612d40a9e9d6285d304c38200d417b1fcf60d17
Alex Geis <siegxela@gmail.com>
2023-01-03 18:10:00 -0700

    add cache-bandit information to projectData.tsx - add gif asset to accompany

--- vite-portfolio/src/pages/home/projects/projectData.tsx
+++ vite-portfolio/src/pages/home/projects/projectData.tsx
@@ -1,35 +1,64 @@
-import pokemonProjectGif from "../../../assets/gifs/pokemon.gif"
-import musicPlayerProjectGif from "../../../assets/gifs/music-player.gif"
-import libraryProjectGif from "../../../assets/gifs/library.gif"
+import pokemonProjectGif from "../../../assets/gifs/pokemon.gif";
+import musicPlayerProjectGif from "../../../assets/gifs/music-player.gif";
+import subTrackerProjectGif from "../../../assets/gifs/cache-bandit.gif";
+// import libraryProjectGif from "../../../assets/gifs/library.gif";
 
 type ProjectDataObjProps = {
-    imgSrc: string;
-    title: string;
-    descArr: string[];
-    gitSrc: string;
-    deployLink: string;
-}
+	imgSrc: string;
+	title: string;
+	descArr: string[];
+	gitSrc: string;
+	deployLink: string;
+};
 
 export const projectData: ProjectDataObjProps[] = [
-    {
-        imgSrc: pokemonProjectGif,
-	    title: "Pokémon Clone",
-	    descArr: ["Move through custom game map","Collision / map boundaries","Sprite animation","Battle sequence w/ monsters","Audio / sound FX","and other features..."],
-	    gitSrc: "https://github.com/alexgeis/pokemon-game",
-	    deployLink: "https://mon-game.netlify.app/"
-    },
-    {
-        imgSrc: musicPlayerProjectGif,
-	    title: "Browser Music Player",
-	    descArr: ["Genre tags","Search filtering","Time/volume controls","Loop + Shuffle","Playlist display","and other features..."],
-	    gitSrc: "https://github.com/alexgeis/music-player",
-	    deployLink: "https://music-player-ang.netlify.app/"
-    },
-    {
-        imgSrc: libraryProjectGif,
-	    title: "Treat Your Shelf",
-	    descArr: ["Login/signup","Auth (node/express + bcrypt + SQL)","Add/delete books","Light/dark theme toggle","Read count progress bar","and other features..."],
-	    gitSrc: "https://github.com/alexgeis/library",
-	    deployLink: "https://github.com/alexgeis/library"
-    }
-]
+	{
+		imgSrc: pokemonProjectGif,
+		title: "Pokémon Clone",
+		descArr: [
+			"Move through custom game map",
+			"Collision / map boundaries",
+			"Sprite animation",
+			"Battle sequence w/ monsters",
+			"Audio / sound FX",
+			"and other features...",
+		],
+		gitSrc: "https://github.com/alexgeis/pokemon-game",
+		deployLink: "https://mon-game.netlify.app/",
+	},
+	{
+		imgSrc: subTrackerProjectGif,
+		title: "Cache Bandit (Subscription Tracker)",
+		descArr: [
+			"Login/signup",
+			"Auth (node/express + bcrypt + MongoDB)",
+			"Add/edit/delete subcriptions",
+			"Light/dark theme toggle",
+			"PWA download",
+			"and other features...",
+		],
+		gitSrc: "https://github.com/alexgeis/SubTracker",
+		deployLink: "https://subtracker.up.railway.app/",
+	},
+	{
+		imgSrc: musicPlayerProjectGif,
+		title: "Browser Music Player",
+		descArr: [
+			"Genre tags",
+			"Search filtering",
+			"Time/volume controls",
+			"Loop + Shuffle",
+			"Playlist display",
+			"and other features...",
+		],
+		gitSrc: "https://github.com/alexgeis/music-player",
+		deployLink: "https://music-player-ang.netlify.app/",
+	},
+	// {
+	//     imgSrc: libraryProjectGif,
+	//     title: "Treat Your Shelf",
+	//     descArr: ["Login/signup","Auth (node/express + bcrypt + SQL)","Add/delete books","Light/dark theme toggle","Read count progress bar","and other features..."],
+	//     gitSrc: "https://github.com/alexgeis/library",
+	//     deployLink: "https://github.com/alexgeis/library"
+	// },
+];
