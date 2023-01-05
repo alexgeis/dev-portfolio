96248db0abde0fbec9ad10e328b348287b9e60e9
Alex Geis <siegxela@gmail.com>
2023-01-04 18:52:12 -0700

    update projectPageData export name and update import in /src/pages/projects/ProjectPageData

--- vite-portfolio/src/pages/projects/ProjectPage.tsx
+++ vite-portfolio/src/pages/projects/ProjectPage.tsx
@@ -5,35 +5,37 @@ import { useState, useEffect, useRef } from "react";
 import { Search } from "../../components/search/Search";
 
 // projects
-import { ProjectPageTemplate } from "./ProjectPageTemplate"
+import { ProjectPageTemplate } from "./ProjectPageTemplate";
 // import { ProjectItem } from "./ProjectItem"
-import { ProjectCard } from "./ProjectCard"
-import { ProjectCardItem } from "./ProjectCardItem"
-import { projectData } from "./projectData";
+import { ProjectCard } from "./ProjectCard";
+import { ProjectCardItem } from "./ProjectCardItem";
+import { projectPageData } from "./projectPageData";
 
 // const fmtMSS = (s) => new Date(1000 * s).toISOString().substr(15, 4);
-const fmtMSS = (s: number) => new Date(1000 * s).toISOString().substring(15, 19);
+const fmtMSS = (s: number) =>
+	new Date(1000 * s).toISOString().substring(15, 19);
 
 export const HomePage = () => {
-    const [query, updateQuery] = useState("");
+	const [query, updateQuery] = useState("");
 
 	return (
 		<>
-            <Search 
-                value={query} 
-                onChange={(e: any) => updateQuery(e.target.value.toLowerCase())}
-                placeholder={`Search ${projectData.length} tracks...`}/>
+			<Search
+				value={query}
+				onChange={(e: any) => updateQuery(e.target.value.toLowerCase())}
+				placeholder={`Search ${projectPageData.length} tracks...`}
+			/>
 			<ProjectPageTemplate>
-				{projectData.map((project, index) => {
+				{projectPageData.map((project, index) => {
 					return (
-						<ProjectCard 
-						key={index}
-						imgSrc={project.imgSrc}
-						title={project.title}
-                        desc={project.desc}
-						deployLink={project.deployLink}
+						<ProjectCard
+							key={index}
+							imgSrc={project.imgSrc}
+							title={project.title}
+							desc={project.desc}
+							deployLink={project.deployLink}
 						/>
-					)
+					);
 				})}
 			</ProjectPageTemplate>
 		</>
