fb63d9dce2457c8146e23313779667167501d005
Alex Geis <siegxela@gmail.com>
2023-01-04 18:51:35 -0700

    updated projectPageData set with tags, add subscription tracker, and add relavent type interface values

--- vite-portfolio/src/pages/projects/projectData.tsx
+++ vite-portfolio/src/pages/projects/projectData.tsx
@@ -1,31 +1,48 @@
-import pokemonProjectGif from "../../../assets/gifs/pokemon.gif"
-import musicPlayerProjectGif from "../../../assets/gifs/music-player.gif"
-import libraryProjectGif from "../../../assets/gifs/library.gif"
+import pokemonProjectGif from "../../../assets/gifs/pokemon.gif";
+import musicPlayerProjectGif from "../../../assets/gifs/music-player.gif";
+import libraryProjectGif from "../../../assets/gifs/library.gif";
+import subTrackerProjectGif from "../../../assets/gifs/cache-bandit.gif";
 
-type ProjectDataObjProps = {
-    imgSrc: string;
-    title: string;
-    desc: string
-    deployLink: string;
-}
+type ProjectPageDataObjProps = {
+	imgSrc: string;
+	title: string;
+	desc: string;
+	techTags: string[];
+	gitSrc: string;
+	deployLink: string;
+};
 
-export const projectData: ProjectDataObjProps[] = [
-    {
-        imgSrc: pokemonProjectGif,
-	    title: "Pokémon Clone",
-        desc: "project description in a few words",
-	    deployLink: "https://mon-game.netlify.app/"
-    },
-    {
-        imgSrc: musicPlayerProjectGif,
-	    title: "Browser Music Player",
-        desc: "project description in a few words",
-	    deployLink: "https://music-player-ang.netlify.app/"
-    },
-    {
-        imgSrc: libraryProjectGif,
-	    title: "Treat Your Shelf",
-        desc: "project description in a few words",
-	    deployLink: "https://github.com/alexgeis/library"
-    }
-]
+export const projectPageData: ProjectPageDataObjProps[] = [
+	{
+		imgSrc: pokemonProjectGif,
+		title: "Pokémon Clone",
+		desc: "project description in a few words",
+		techTags: ["JavaScript"],
+		gitSrc: "https://github.com/alexgeis/pokemon-game",
+		deployLink: "https://mon-game.netlify.app/",
+	},
+	{
+		imgSrc: subTrackerProjectGif,
+		title: "Cache Bandit (Subscription Tracker)",
+		desc: "Manage and budget ongoing subscriptions",
+		techTags: ["JavaScript", "React", "Webpack", "MongoDB"],
+		gitSrc: "https://github.com/alexgeis/SubTracker",
+		deployLink: "https://subtracker.up.railway.app/",
+	},
+	{
+		imgSrc: musicPlayerProjectGif,
+		title: "Browser Music Player",
+		desc: "project description in a few words",
+		techTags: ["JavaScript", "React"],
+		gitSrc: "https://github.com/alexgeis/music-player",
+		deployLink: "https://music-player-ang.netlify.app/",
+	},
+	{
+		imgSrc: libraryProjectGif,
+		title: "Treat Your Shelf",
+		desc: "project description in a few words",
+		techTags: ["JavaScript", "MySQL"],
+		gitSrc: "https://github.com/alexgeis/library",
+		deployLink: "https://github.com/alexgeis/library",
+	},
+];
