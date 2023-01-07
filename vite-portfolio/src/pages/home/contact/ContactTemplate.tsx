9ec8a6e981deb3a29f166eb1a00e941f6ae09963
Alex Geis <siegxela@gmail.com>
2023-01-07 10:28:50 -0700

    update home/hero css modules with new index.css color variables

--- vite-portfolio/src/pages/home/contact/ContactTemplate.tsx
+++ vite-portfolio/src/pages/home/contact/ContactTemplate.tsx
@@ -2,12 +2,13 @@ import styles from "./ContactTemplate.module.css";
 import { NavLink } from "react-router-dom";
 
 export const ContactTemplate = () => {
-	return <div className={styles.wrapper}>
-				<h1 className={styles.contactTitle}>Have a project in mind?</h1>
-				<p className={styles.contactDesc}>I'm available to work!</p>
-				<NavLink 
-                    to="contact">
-					<p className={styles.contactBtn}>Contact Me</p>
-                </NavLink>
-		   </div>;
+	return (
+		<div className={styles.wrapper}>
+			<h1 className={styles.contactTitle}>Let's start our next project</h1>
+			<p className={styles.contactDesc}>I'm available to work!</p>
+			<NavLink to="contact">
+				<p className={styles.contactBtn}>Contact Me</p>
+			</NavLink>
+		</div>
+	);
 };
