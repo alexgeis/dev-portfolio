c964f5ea13117868d53db3ba621bc0db58f46004
Alex Geis <siegxela@gmail.com>
2023-02-02 21:22:04 -0700

    add scroll to top button

--- vite-portfolio/src/App.tsx
+++ vite-portfolio/src/App.tsx
@@ -1,23 +1,25 @@
 // import { useState } from 'react'
-import styles from './App.module.css'
+import styles from "./App.module.css";
 
 import { Outlet } from "react-router-dom";
 
 import { NavBar } from "./components/NavBar";
 import { Footer } from "./components/Footer";
+import ScrollToTop from "./components/ScrollToTop";
 
 type Props = {
-	children?: React.ReactNode
+	children?: React.ReactNode;
 };
 
 export const AppTemplate: React.FC<Props> = ({ children }) => {
-  return (
-        <div className={styles.wrapper}>
-          <NavBar />
-          {children}
-          <Outlet />
-          <div className={styles.spacer}></div>
-          <Footer />
-        </div>
-  )
-}
+	return (
+		<div className={styles.wrapper}>
+			<NavBar />
+			<ScrollToTop />
+			{children}
+			<Outlet />
+			<div className={styles.spacer}></div>
+			<Footer />
+		</div>
+	);
+};
--- vite-portfolio/src/components/ScrollToTop.tsx
+++ vite-portfolio/src/components/ScrollToTop.tsx
@@ -1,9 +1,40 @@
 import styles from "./ScrollToTop.module.css";
-import slideUpArrow from "../assets/icons/scrollToTop/slide-up-white.png"
+import { useState, useEffect } from "react";
+import slideUpArrow from "../assets/icons/scrollToTop/slide-up-white.png";
 
+export default function ScrollToTop() {
+	const [btnShow, setBtnShow] = useState<boolean>(false);
 
-export const ScrollToTop = () => {
-	return <div className={styles.wrapper}>
-				<img src={slideUpArrow} alt="slide up arrow" />
-		   </div>;
-};
+	useEffect(() => {
+		const handleScrollBtnShow = () => {
+			window.scrollY > 100 ? setBtnShow(true) : setBtnShow(false);
+		};
+
+		window.addEventListener("scroll", handleScrollBtnShow);
+
+		return () => {
+			window.removeEventListener("scroll", handleScrollBtnShow);
+		};
+	}, []);
+
+	function handleScrollToTop() {
+		window.scrollTo({ top: 0, behavior: "smooth" });
+	}
+	return (
+		<>
+			{btnShow && (
+				<button
+					onClick={handleScrollToTop}
+					id="myBtn"
+					className={styles.myBtn}
+					title="Go to top"
+				>
+					<img
+						src={slideUpArrow}
+						alt="scroll to top arrow"
+					/>
+				</button>
+			)}
+		</>
+	);
+}
